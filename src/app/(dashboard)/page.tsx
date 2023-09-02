6af00508c170f54ba02539aee902790c5272eca1
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-09-02 15:07:18 +0530

    Add collection list component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,22 @@
+import CreateCollectionButton from '@/components/CreateCollectionButton'
+import SadFace from '@/components/icons/SadFace'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Skeleton } from '@/components/ui/skeleton'
+import prisma from '@/lib/prisma'
 import { wait } from '@/lib/wait'
 import { currentUser } from '@clerk/nextjs'
 import { Suspense } from 'react'
 
 export default async function Home() {
   return (
-    <Suspense fallback={<WelcomeMsgFallback />}>
-      <Welcome />
-    </Suspense>
+    <>
+      <Suspense fallback={<WelcomeMsgFallback />}>
+        <Welcome />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CollectionList />
+      </Suspense>
+    </>
   )
 }
 
@@ -19,7 +28,7 @@ async function Welcome() {
     return <div>error</div>
   }
   return (
-    <div className='flex w-full'>
+    <div className='flex w-full mb-12'>
       <h1 className='text-4xl font-bold'>
         Welcome, <br /> {user.firstName}
       </h1>
@@ -29,11 +38,35 @@ async function Welcome() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className='flex w-full'>
-      <h1 className='text-4xl font-bold'>
+    <div className='flex w-full mb-12'>
+      <h1 className='text-4xl font-bold space-y-2'>
         <Skeleton className='w-[150px] h-[36px]' />
         <Skeleton className='w-[150px] h-[36px]' />
       </h1>
     </div>
   )
+}
+
+async function CollectionList() {
+  const user = await currentUser()
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  })
+
+  if (collections.length === 0) {
+    return (
+      <div className='flex flex-col gap-5 mt-4'>
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet</AlertTitle>
+          <AlertDescription>
+            Create a Collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionButton />
+      </div>
+    )
+  }
 }
