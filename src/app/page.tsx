0421981a9e853b16e9690bd028b021c3a5818afd
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-09-02 10:16:56 +0530

    style themeSwitcher component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
+import { currentUser } from '@clerk/nextjs'
 
-export default function Home() {
-  return <div>hello</div>
+export default async function Home() {
+  const user = await currentUser()
+  if (!user) {
+    return <div>error</div>
+  }
+  return (
+    <div>
+      Welcome, <br /> {user.firstName}
+    </div>
+  )
 }
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,11 +1,51 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { Tabs, TabsList, TabsTrigger } from './ui/tabs'
+import { DesktopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
 
-  return <div>ThemeSwitcher</div>
+  // When mounted on client, now we can show the UI, this avoid hydration errors
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className='border dark:border-neutral-800 dark:bg-[#030303]'>
+        <TabsTrigger
+          value='light'
+          onClick={(e) => {
+            setTheme('light')
+          }}
+        >
+          <SunIcon className='h-[1.2rem] w-[1.2rem]' />
+        </TabsTrigger>
+        <TabsTrigger
+          value='dark'
+          onClick={(e) => {
+            setTheme('dark')
+          }}
+        >
+          <MoonIcon className='h-[1.2rem] w-[1.2rem] rotate-90 trasition-all dark:rotate-0' />
+        </TabsTrigger>
+        <TabsTrigger
+          value='system'
+          onClick={(e) => {
+            setTheme('system')
+          }}
+        >
+          <DesktopIcon className='h-[1.2rem] w-[1.2rem]' />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  )
 }
 
 export default ThemeSwitcher
